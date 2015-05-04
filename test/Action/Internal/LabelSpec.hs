a9aba10e1fceedc9b6ce27027f26caf3655bfc9f
Scott Murphy <scottmurphy09@gmail.com>
2015-05-03 20:40:29 -0500

    Round trip tests on label creation... no network test, sadly

--- test/Action/Internal/LabelSpec.hs
+++ test/Action/Internal/LabelSpec.hs
@@ -1,18 +1,33 @@
 {-# LANGUAGE TemplateHaskell #-}
 module Action.Internal.LabelSpec (main, spec) where
 
-import           Action.Internal.Label (RepoLabel (..), addLabelsToRepo,
-                                        fromLabelColorString, fromLabelString,
+import           Action.Internal.Label (RepoLabel, fromLabelColorString,
+                                        fromLabelString, labelList,
                                         toLabelColorString, toLabelString)
+import           Control.Applicative
+import           Data.Either
 import           Language.Haskell.TH   (nameBase)
 import           Test.Hspec
+
 main :: IO ()
 main = hspec spec
 
 -- | This spec is just here to make sure the API stays consistant when I add and remove files
 
+
+
+lst :: [RepoLabel]
+lst = labelList
+
 spec :: Spec
 spec = do
-  describe (nameBase 'addLabelsToRepo) $ do
-    it "should be exported by Action Label" $ do
-      True `shouldBe` True
+  describeTH ['fromLabelColorString , 'toLabelColorString] $
+    it "should be exported by Action Label and return" $
+      shouldBe  lst
+               (rights . fmap (fromLabelColorString.toLabelColorString) $ lst)
+  describeTH ['fromLabelString, 'toLabelString] $
+    it "should be exported by Action Label and return" $
+      shouldBe  lst
+               (rights . fmap (fromLabelString.toLabelString) $ lst)
+ where
+   describeTH names = describe (unwords $ nameBase <$> names)
