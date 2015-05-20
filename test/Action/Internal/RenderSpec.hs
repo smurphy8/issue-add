c04e7ccd1d04d63830a446637e770226541eee76
Scott Murphy <scottmurphy09@gmail.com>
2015-05-20 02:07:29 -0500

    Fixed point on rendering and parsing creaed

--- test/Action/Internal/RenderSpec.hs
+++ test/Action/Internal/RenderSpec.hs
@@ -12,37 +12,60 @@ import           Control.Lens                           (folded, over, toListOf,
                                                          (.~))
 import           Data.Either
 import           Data.String.Here                       (here)
-import           Data.Text                              (pack)
+import           Data.Text                              (Text, pack)
 import           Language.Haskell.TH                    (nameBase)
 import           Test.Hspec
 import           Test.QuickCheck
 
+import           Control.Monad.Fix
 import           Data.OrgMode.Parse.Attoparsec.Document
 import           Data.OrgMode.Parse.Types
 
-
 -- | Heading Golden Test Code
 
-headingExample = "* TODO [#A] This is the issue title [0%] :Bug:\n** ASSIGN  @smurphy8  \n\n**   Events  \n\n\n"
+headingExample = [here|
+* TODO [#A] This is the issue title [0%] :Bug:
+** ASSIGN  @smurphy8
+**   Events|]
+
+
+-- | SOme really dirty functions to get me what I need to make all this work
 
 eitherExample = parseOrgMode headingExample
 (h:headings) = documentHeadings doc
   where
    (Right doc)= eitherExample
 
-
-
-
+-- |little sugar for the naming
 describeTH names = describe (unwords $ nameBase <$> names)
+
+-- | functions look for a fixed point to establish an adjunction
+-- | Fix point on  Rendering
+fixRenderFunction  = fmap (renderHeading.head.documentHeadings) .
+                                          parseOrgMode
+-- | Fixed point on parsing
+fixHeadingFunction = fmap (head.documentHeadings) . parseOrgMode .
+                     renderHeading
+
+
+runFixN f n' i' = either (const False) (const True) $ loop n' i'
+   where
+    loop n i = do
+     v <- f i
+     if (v == i)
+     then return i
+     else if (n <= 0)
+          then Left "Failed to fix"
+          else loop (n-1) v
+
+
+
 
 main = hspec spec
 
 spec :: Spec
 spec = do
    describeTH ['renderHeading] $ do
-     it "should parse a golden test and render it back consistently" $ do
-        let (Right h' ) = fmap (head.documentHeadings) .
-                               parseOrgMode . renderHeading $ h
-        headingExample `shouldBe` renderHeading h
-        h' `shouldBe` h
-
+     it "checks a simple example for a fixed point on parsing and rendering" $ do
+        True `shouldBe` (runFixN fixRenderFunction 5 headingExample)
+        True `shouldBe` runFixN fixHeadingFunction 5 h
